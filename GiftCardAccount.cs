93d821fb9dda666ffa30261651c20938d3a09eb0
cutetory <101031776+cutetory@users.noreply.github.com>
2022-03-18 00:09:33 +0900

    메인에 new giftcard 추가

--- GiftCardAccount.cs
+++ GiftCardAccount.cs
@@ -8,6 +8,15 @@ namespace BNK
     {
         private readonly decimal _monthlyDeposit = 0m;
 
-        public GiftCardAccount(string name, decimal initialBalance, decimal monthlyDeposit = 0) : base(name, initialBalance) => _monthlyDeposit = monthlyDeposit;
+        public GiftCardAccount(string name, decimal initialBalance, decimal monthlyDeposit = 0) : base(name, initialBalance)
+            => _monthlyDeposit = monthlyDeposit;
+
+        public override void PerformMonthEndTransactions()
+        {
+            if (_monthlyDeposit != 0)
+            {
+                MakeDeposit(_monthlyDeposit, DateTime.Now, "Add monthly deposit");
+            }
+        }
     }
 }
